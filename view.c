92ab517a114a16d9138aaac6dc5bee467eab41f2
Basil Nikityuk <basil.nikityuk@gmail.com>
2025-08-23 17:02:04 +0300

    up

--- view.c
+++ view.c
@@ -793,7 +793,7 @@ void V_SetPalette (byte *palette)
 			*table++ = v;
 
 			// tint part, FIXME not used?
-			RTint[i*16+p] = ((float)r) / ((float)ColorPercent[15-p]) ;
+			RTint[i*16+p] = ((float)r) / ((float)ColorPercent[15-p]);
 			GTint[i*16+p] = ((float)g) / ((float)ColorPercent[15-p]);
 			BTint[i*16+p] = ((float)b) / ((float)ColorPercent[15-p]);
 		}
