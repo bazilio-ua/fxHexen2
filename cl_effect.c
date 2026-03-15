f357b9a8f1965aea832d7f8eba3f16e5650b6687
Basil Nikityuk <basil.nikityuk@gmail.com>
2026-03-15 12:37:24 +0200

    update effects

--- cl_effect.c
+++ cl_effect.c
@@ -831,6 +831,17 @@ void CL_ParseEffect(void)
 				}
 				else if (cl.Effects[index].type == CE_MAGIC_MISSILE_EXPLOSION)
 				{
+					if (cl_extradlight.value)
+					{
+						dl = CL_AllocDlight (0);
+						VectorCopy (ent->origin, dl->origin);
+						dl->radius = 150;
+						dl->die = cl.time + 0.5;
+						dl->decay = 300;
+						
+						CL_ColorDlightPaletteIndices (dl, DL_COLOR_MM_EXPLOSION);
+					}
+
 					ent->model = Mod_ForName("models/mm_expld.spr", true);
 				}
 				else if (cl.Effects[index].type == CE_BONE_EXPLOSION)
--- client.h
+++ client.h
@@ -207,6 +207,7 @@ typedef struct
 #define DL_COLOR_G_BLUE		247, 4	// glow blue
 #define DL_COLOR_G_ORANGE	251, 4	// glow orange
 
+#define DL_COLOR_MM_EXPLOSION	(int []) {28, 31, 47, 159}, 4
 #define DL_COLOR_MEZZO_REF	(int []) {135, 139, 140, 143}, 4
 #define DL_COLOR_RED_ARROW	(int []) {135, 137, 140, 143}, 4	// red arrow, skin1
 
