cbd640f91ddfc886a157480bfb75687fb837f462
Basil Nikityuk <basil.nikityuk@gmail.com>
2026-04-01 14:40:49 +0300

    PR_PrintStatement indent formatting

--- pr_exec.c
+++ pr_exec.c
@@ -108,45 +108,37 @@ void PR_PrintStatement (dstatement_t *s)
 {
 	int i;
 
-	if((unsigned)s->op < sizeof(pr_opnames)/sizeof(pr_opnames[0]))
+	if ( (unsigned)s->op < sizeof(pr_opnames)/sizeof(pr_opnames[0]))
 	{
-		Con_Printf("%s ", pr_opnames[s->op]);
+		Con_SafePrintf ("%s ", pr_opnames[s->op]);
 		i = strlen(pr_opnames[s->op]);
-		for(; i < 10; i++)
-		{
-			Con_Printf(" ");
-		}
+		for ( ; i<10 ; i++)
+			Con_Printf (" ");
 	}
 
-	if(s->op == OP_IF || s->op == OP_IFNOT)
+	if (s->op == OP_IF || s->op == OP_IFNOT)
 	{
-		Con_Printf("%sbranch %i", PR_GlobalString(s->a), s->b);
+		Con_SafePrintf ("%sbranch %i", PR_GlobalString(s->a), s->b);
 	}
-	else if(s->op == OP_GOTO)
+	else if (s->op == OP_GOTO)
 	{
-		Con_Printf("branch %i", s->a);
+		Con_SafePrintf ("branch %i", s->a);
 	}
-	else if((unsigned)(s->op-OP_STORE_F) < 6)
+	else if ( (unsigned)(s->op - OP_STORE_F) < 6)
 	{
-		Con_Printf("%s", PR_GlobalString(s->a));
-		Con_Printf("%s", PR_GlobalStringNoContents(s->b));
+		Con_SafePrintf ("%s", PR_GlobalString(s->a));
+		Con_SafePrintf ("%s", PR_GlobalStringNoContents(s->b));
 	}
 	else
 	{
-		if(s->a)
-		{
-			Con_Printf("%s", PR_GlobalString(s->a));
-		}
-		if(s->b)
-		{
-			Con_Printf("%s", PR_GlobalString(s->b));
-		}
-		if(s->c)
-		{
-			Con_Printf("%s", PR_GlobalStringNoContents(s->c));
-		}
+		if (s->a)
+			Con_SafePrintf ("%s", PR_GlobalString(s->a));
+		if (s->b)
+			Con_SafePrintf ("%s", PR_GlobalString(s->b));
+		if (s->c)
+			Con_SafePrintf ("%s", PR_GlobalStringNoContents(s->c));
 	}
-	Con_Printf("\n");
+	Con_SafePrintf ("\n");
 }
 
 /*
