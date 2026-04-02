fc89bb44cfd7ac030afb90cbb80fa772ebad7a48
Basil Nikityuk <basil.nikityuk@gmail.com>
2026-04-02 19:25:35 +0300

    added header license for midi stub

--- midi_null.c
+++ midi_null.c
@@ -1,32 +1,56 @@
+/*
+Copyright (C) 1996-1997 Id Software, Inc.
+Copyright (C) 1997-1998 Raven Software, Inc.
 
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+// midi_null.c -- Midi Stuff
 
 #include "quakedef.h"
 
 
 qboolean MIDI_Init(void)
 {
-	Con_Printf("MIDI_DRV: disabled at compile time.\n");
+	Con_Printf ("MIDI not implemented\n");
 	return false;
 }
 
-
-void MIDI_Play(char *Name)
+void MIDI_Play (char *Name)
 {
+	
 }
 
-void MIDI_Pause(void)
+void MIDI_Pause (void)
 {
+	
 }
 
-void MIDI_Loop(int NewValue)
+void MIDI_Loop (int NewValue)
 {
+	
 }
 
-void MIDI_Stop(void)
+void MIDI_Stop (void)
 {
+	
 }
 
-void MIDI_Cleanup(void)
+void MIDI_Cleanup (void)
 {
+	
 }
 
