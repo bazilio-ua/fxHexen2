2635e88f1cedfda764e0e5ae3387b1d34698671e
Basil Nikityuk <basil.nikityuk@gmail.com>
2026-04-07 20:30:43 +0300

    show illegible message number

--- cl_parse.c
+++ cl_parse.c
@@ -1209,7 +1209,7 @@ void CL_ParseServerMessage (void)
 		switch (cmd)
 		{
 		default:
-			Host_Error ("CL_ParseServerMessage: Illegible server message\n");
+			Host_Error ("CL_ParseServerMessage: Illegible server message (%d)\n", cmd);
 			break;
 			
 		case svc_nop:
