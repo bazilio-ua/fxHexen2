0a308cecad06ceffc574c71888a21f6b96fe8187
Basil Nikityuk <basil.nikityuk@gmail.com>
2026-04-20 00:37:53 +0300

    indentation

--- vid_cgl.m
+++ vid_cgl.m
@@ -617,7 +617,7 @@ void VID_DescribeModes_f (void)
 }
 
 
-static int vmodecompare(const void *inmode1, const void *inmode2)
+int vmodecompare(const void *inmode1, const void *inmode2)
 {
 	// sort lowest res to highest
 	const vmode_t *mode1 = (vmode_t *)inmode1;
@@ -931,36 +931,36 @@ void VID_Shutdown (void)
 {
 	CGError err;
 	
-    if (display) {
-        
-        if (glcontext) {
-            [NSOpenGLContext clearCurrentContext];
-            
-            [glcontext clearDrawable];
-            
-            [glcontext release];
-            glcontext = nil;
-        }
-        
-        if (window) {
-            [window release];
-            window = nil;
-        }
-        
-        if (screen) {
-            [screen release];
-            screen = nil;
-        }
-        
-        // Switch back to the original screen resolution
-        if (vid.fullscreen) {
-            if (desktopMode) {
+	if (display) {
+		
+		if (glcontext) {
+			[NSOpenGLContext clearCurrentContext];
+			
+			[glcontext clearDrawable];
+			
+			[glcontext release];
+			glcontext = nil;
+		}
+		
+		if (window) {
+			[window release];
+			window = nil;
+		}
+		
+		if (screen) {
+			[screen release];
+			screen = nil;
+		}
+		
+		// Switch back to the original screen resolution
+		if (vid.fullscreen) {
+			if (desktopMode) {
 				err = CGDisplaySetDisplayMode(display, desktopMode, NULL); // Restoring desktop mode
 				if (err != kCGErrorSuccess)
 					Con_Printf("Unable to restore display mode\n");
-            }
-        }
-        
+			}
+		}
+		
 		// Release the main display
 		if (vid.fullscreen) {
 			if (CGDisplayIsMain(display)) {
@@ -977,9 +977,9 @@ void VID_Shutdown (void)
 		
 		if (displayModes)
 			CFRelease (displayModes);
-    }
-    
-    vid.fullscreen = false;
+	}
+	
+	vid.fullscreen = false;
 }
 
 //==========================================================================
